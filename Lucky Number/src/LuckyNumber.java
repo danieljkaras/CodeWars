8c2b6aa9f94af6ae2af57bcf98976c0e87d7a10f
Daniel Karaś <daniel.j.karas@gmail.com>
2018-04-17 09:46:33 +0200

    code wars kata lucky number fix

--- Lucky Number/src/LuckyNumber.java
+++ Lucky Number/src/LuckyNumber.java
@@ -2,15 +2,12 @@ public class LuckyNumber {
 
     public boolean isLucky(long n) {
 
-        int sum = String.valueOf(n)
-                .chars()
-                .map(Character::getNumericValue)
-                .sum();
-
         boolean result;
+        int sum = String.valueOf(n).chars().map(Character::getNumericValue).sum();
+        System.out.println("Value of suming is: " + sum);
 
         if (sum == 0 || sum % 9 == 0) {
-            System.out.println("You are lucky man!!");
+            System.out.println("You are lucky man!!!");
             result = true;
         } else {
             System.out.println("You are not lucky man!!!");
